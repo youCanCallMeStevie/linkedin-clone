0fb0fb1309f36d3a76564bb07f7951dfa2d99f56
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2020-11-30 12:22:26 +0100

    adds assets/logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Profile from './Components/Profile';
+import NavBar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Profile/>
+      <NavBar />
     </div>
   );
 }
