a27d775952e9de26f5fc50ee6cb32ab98f6d39a0
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2020-12-01 17:38:34 +0100

    Update ExperienceEducation.jsx

--- src/Components/ExperienceEducation.jsx
+++ src/Components/ExperienceEducation.jsx
@@ -1,332 +1,201 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Card, Row, Image, Col, Media } from "react-bootstrap";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-  Divider,
-} from "@material-ui/core";
-import AddIcon from "@material-ui/icons/Add";
-import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
-import CreateIcon from "@material-ui/icons/Create";
+import { Container, Form, Row, Col, Modal, Button } from "react-bootstrap";
+import { postExperiences } from "../utils";
 
-import "../Styles/ExperienceEducation.css";
+export class ExperienceModal extends React.Component {
+  state = {
+    validated: false,
+    setValidated: false,
+    experience: {
+area: "London",
+company: "Lou Taylor",
+description: "jewellery",
+endDate: "",
+role: "Manager",
+startDate: "2019-06-12",
+    },
+  };
 
-export default function Promoted() {
-  return (
-    <div className="mt-3">
-      <Card className="experience-education-container">
-        <Card.Body>
-          <Col>
-            <Row className="justify-content-between">
-              <Col className="d-flex justify-content-start">
-                <Card.Title classname="card-title-expereince d-flex justify-content-start">
-                  Experience
-                </Card.Title>
+  updateExp = event => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      this.setState({ validated: true });
+    }
+  };
+
+  handleChange = e => {
+    let newExperience = { ...this.state.experience };
+    newExperience[e.target.name] = e.target.value;
+    this.setState({ experience: newExperience });
+    console.log(this.state.experience);
+  };
+
+  handleSubmit =  async (e) => {
+    e.preventDefault(e)
+  
+  await postExperiences(this.props.userId, this.state.experience)
+      console.log("button is working")
+
+
+  }
+
+  render() {
+    const { toggleModal, showModal, userId } = this.props;
+
+    return (
+      <Modal
+        show={showModal}
+        onHide={toggleModal}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add Experience</Modal.Title>
+        </Modal.Header>
+        <Container>
+          <Form className="text-white mt-5" onSubmit={(e)=>this.handleSubmit(e)}>
+            <Form.Group>
+            <Form.Text className="text-muted">
+            Title *
+            </Form.Text>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Ex: Retail Sales Manager"
+                name="role"
+                value={this.state.experience.role}
+                onChange={e => {
+                  this.handleChange(e);
+                }}
+              />
+            </Form.Group>
+            <Form.Group>
+            <Form.Text className="text-muted">
+            Employment Type
+            </Form.Text>
+              <Form.Control
+                as="select"
+                name="EmploymentType"
+                EmploymentType="EmploymentType"
+                custom
+              >
+                <option value="0">Full-Time</option>
+                <option value="1">Part-Time</option>
+                <option value="2">Self Employed</option>
+                <option value="3">Freelance</option>
+                <option value="4">Contract</option>
+                <option value="5">Internship</option>
+                <option value="6">Apprenticeship</option>
+              </Form.Control>
+              <Form.Text className="text-muted">
+                Country-specific employment types
+              </Form.Text>
+            </Form.Group>
+            <Form.Group>
+            <Form.Text className="text-muted">
+            Company *
+            </Form.Text>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Ex: Microsoft"
+                name="company"
+                value={this.state.experience.company}
+                onChange={e => {
+                  this.handleChange(e);
+                }}
+              />
+            </Form.Group>
+            <Form.Group>
+            <Form.Text className="text-muted">
+            Location
+            </Form.Text>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Ex: London, United Kingdom"
+                name="area"
+                value={this.state.experience.area}
+
+                onChange={e => {
+                  this.handleChange(e);
+                }}
+              />
+            </Form.Group>
+            <Form.Group >
+              <Form.Check
+                type="checkbox"
+                label="I am currently working in this role"
+              />
+            </Form.Group>
+            <Row>
+              <Col>
+                <Form.Group>
+                <Form.Text className="text-muted" htmlFor="dateTime">
+            Start Date
+            </Form.Text>
+                  <Form.Control
+                    type="date"
+                    name="startDate"
+                    placeholder="Start Date"
+                    value={this.state.experience.startDate}
+                    onChange={e => {
+                      this.handleChange(e);
+                    }}
+                    required
+                  />
+                </Form.Group>
               </Col>
 
-              <Col className="d-flex justify-content-end">
-                <AddIcon />
+              <Col>
+                <Form.Text className="text-muted" htmlFor="dateTime">
+            End Date
+            </Form.Text>
+                <Form.Group>
+                  <Form.Control
+                    type="date"
+                    name="endDate"
+                    placeholder="End Date"
+                    value={this.state.experience.endDate}
+
+                    onChange={e => {
+                      this.handleChange(e);
+                    }}
+                  />
+                </Form.Group>
               </Col>
             </Row>
-          </Col>
-
-          <List>
-            <ListItem>
-              <div>
-                <Image
-                  src="https://via.placeholder.com/90x90"
-                  thumbnail
-                  className="experience-education-avatars mr-3"
-                />
-              </div>
-              <Col>
-                <Row className="justify-content-between">
-                  <Typography variant="h5">Job Position</Typography>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                </Row>
-                <Row>
-                  <ListItemText
-                    primary="Company Name"
-                    secondary="Time at Position"
-                  />
-                </Row>
-              </Col>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <div>
-                <Image
-                  src="https://via.placeholder.com/90x90"
-                  thumbnail
-                  className="experience-education-avatars mr-3"
-                />
-              </div>
-              <Col>
-                <Row className="justify-content-between">
-                  <Typography variant="h5">Job Position</Typography>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                </Row>
-                <Row>
-                  <ListItemText
-                    primary="Company Name"
-                    secondary="Time at Position"
-                  />
-                </Row>
-              </Col>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <div>
-                <Image
-                  src="https://via.placeholder.com/90x90"
-                  thumbnail
-                  className="experience-education-avatars mr-3"
-                />
-              </div>
-              <Col>
-                <Row className="justify-content-between">
-                  <Typography variant="h5">Job Position</Typography>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                </Row>
-                <Row>
-                  <ListItemText
-                    primary="Company Name"
-                    secondary="Time at Position"
-                  />
-                </Row>
-              </Col>
-            </ListItem>
-          </List>
-        </Card.Body>
-        <ListItem
-          button="primary"
-          component="a"
-          href="#"
-          className="show-more-list-link justify-content-start"
-        >
-          Show 3 more expereinces
-          <KeyboardArrowDownIcon />
-        </ListItem>
-        <Divider light />
-
-        <Card.Body>
-          <Col>
-            <Row className="justify-content-between">
-              <Col className="d-flex justify-content-start">
-                <Card.Title classname="card-title-expereince d-flex justify-content-start">
-                  Education
-                </Card.Title>
-              </Col>
-
-              <Col className="d-flex justify-content-end">
-                <AddIcon />
-              </Col>
-            </Row>
-          </Col>
-
-          <List>
-            <ListItem>
-              <div>
-                <Image
-                  src="https://via.placeholder.com/90x90"
-                  thumbnail
-                  className="experience-education-avatars mr-3"
-                />
-              </div>
-              <Col>
-                <Row className="justify-content-between">
-                  <Typography variant="h5">School</Typography>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                </Row>
-                <Row>
-                  <ListItemText
-                    primary="Education Title"
-                    secondary="Length of Studies"
-                  />
-                </Row>
-                <Row>
-                  <ListItemText secondary="Scholarly Merits" />
-                </Row>
-              </Col>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <div>
-                <Image
-                  src="https://via.placeholder.com/90x90"
-                  thumbnail
-                  className="experience-education-avatars mr-3"
-                />
-              </div>
-              <Col>
-                <Row className="justify-content-between">
-                  <Typography variant="h5">School</Typography>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                </Row>
-                <Row>
-                  <ListItemText
-                    primary="Education Title"
-                    secondary="Length of Studies"
-                  />
-                </Row>
-                <Row>
-                  <ListItemText secondary="Scholarly Merits" />
-                </Row>
-              </Col>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <div>
-                <Image
-                  src="https://via.placeholder.com/90x90"
-                  thumbnail
-                  className="experience-education-avatars mr-3"
-                />
-              </div>
-              <Col>
-                <Row className="justify-content-between">
-                  <Typography variant="h5">School</Typography>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                </Row>
-                <Row>
-                  <ListItemText
-                    primary="Education Title"
-                    secondary="Length of Studies"
-                  />
-                </Row>
-                <Row>
-                  <ListItemText secondary="Scholarly Merits" />
-                </Row>
-              </Col>
-            </ListItem>
-          </List>
-        </Card.Body>
-        <ListItem
-          button="primary"
-          component="a"
-          href="#"
-          className="show-more-list-link justify-content-start"
-        >
-          Show 3 more education
-          <KeyboardArrowDownIcon />
-        </ListItem>
-        <Divider light />
-
-        <Card.Body>
-          <Col>
-            <Row className="justify-content-between">
-              <Col className="d-flex justify-content-start">
-                <Card.Title classname="card-title-expereince d-flex justify-content-start">
-                  Licenses & Certifications
-                </Card.Title>
-              </Col>
-
-              <Col className="d-flex justify-content-end">
-                <AddIcon />
-              </Col>
-            </Row>
-          </Col>
-
-          <List>
-            <ListItem>
-              <div>
-                <Image
-                  src="https://via.placeholder.com/90x90"
-                  thumbnail
-                  className="experience-education-avatars mr-3"
-                />
-              </div>
-              <Col>
-                <Row className="justify-content-between">
-                  <Typography variant="h5">Name of Award</Typography>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                </Row>
-                <Row>
-                  <ListItemText
-                    primary="Earned from Where"
-                    secondary="Time of Issue"
-                  />
-                </Row>
-              </Col>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <div>
-                <Image
-                  src="https://via.placeholder.com/90x90"
-                  thumbnail
-                  className="experience-education-avatars mr-3"
-                />
-              </div>
-              <Col>
-                <Row className="justify-content-between">
-                  <Typography variant="h5">Name of Award</Typography>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                </Row>
-                <Row>
-                  <ListItemText
-                    primary="Earned from Where"
-                    secondary="Time of Issue"
-                  />
-                </Row>
-              </Col>
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            <ListItem>
-              <div>
-                <Image
-                  src="https://via.placeholder.com/90x90"
-                  thumbnail
-                  className="experience-education-avatars mr-3"
-                />
-              </div>
-              <Col>
-                <Row className="justify-content-between">
-                  <Typography variant="h5">Name of Award</Typography>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                </Row>
-                <Row>
-                  <ListItemText
-                    primary="Earned from Where"
-                    secondary="Time of Issue"
-                  />
-                </Row>
-              </Col>
-            </ListItem>
-          </List>
-        </Card.Body>
-        <ListItem
-          button="primary"
-          component="a"
-          href="#"
-          className="show-more-list-link justify-content-start"
-        >
-          Show more
-          <KeyboardArrowDownIcon />
-        </ListItem>
-      </Card>
-    </div>
-  );
+            <Form.Text className="text-muted">
+            Description
+            </Form.Text>
+            <Form.Group >
+              <Form.Control
+                required
+                name="description"
+                value={this.state.experience.description}
+                onChange={e => {
+                  this.handleChange(e);
+                }}
+                as="textarea"
+                rows={3}
+              />
+            </Form.Group>
+            {/* <Form.File id="uploadFile">
+              <Form.File.Label>Upload</Form.File.Label>
+              <Form.File.Input />
+            </Form.File> */}
+                    
+          </Form>
+        </Container>
+        <Modal.Header>
+          <Modal.Title><Button type="submit" variant="primary" >Submit</Button></Modal.Title>
+        </Modal.Header>
+      </Modal>
+    );
+  }
 }
+
+export default ExperienceModal;
