71e309f7e59bf8ce69db5ef27645f699ba90605d
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2020-12-02 07:19:02 -0500

    witUser

--- src/Components/Feeds.jsx
+++ src/Components/Feeds.jsx
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 import { Container, Col, Row } from "react-bootstrap";
+import  withUser from "./withUser";
 import "../Styles/Feeds.css";
 import { fetchUser, fetchAllUsers, fetchExperiences } from "../utils";
+import FeedsProfileCard from "./FeedsProfileCard";
 import ProfileDetailsCard from "./ProfileDetailsCard";
 
-export default class Feeds extends Component {
-  render() {
+class Feeds extends Component {
+    state = {
+        user: "",
+        allUsers:[]
+    }
+  componentDidMount = () => {
+    console.log(this.props);
+  };
+
+    componentDidUpdate = (prevProp) => {
+        if (prevProp.curretUser !== this.props.curretUser) {
+            this.setState({ user: this.props.curretUser });
+        }
+        if (prevProp.allUsers !== this.props.allUsers) {
+              this.setState({ allUsers: this.props.allUsers });
+            }
+
+    }
+    render() {
+      const { user, allUsers } = this.state;
     return (
       <Container className="feeds">
         <Row>
-          <Col md={3}>{/* here goes the profile card - Rita - */}</Col>
+          <Col md={3}>
+                    {/* here goes the profile card - Rita - */}
+                    {user?.name}
+                    <FeedsProfileCard user={user} users={allUsers}/>
+          </Col>
           <Col md={5}> {/* here goes all feeds + create new feed - */}</Col>
           <Col md={4}> {/* here goes the small list of recent feeds - */}</Col>
         </Row>
@@ -17,3 +41,5 @@ export default class Feeds extends Component {
     );
   }
 }
+
+export default withUser(Feeds);
