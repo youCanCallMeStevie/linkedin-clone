4e6571832db30a77cb6caf393fdab4bd8891482a
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2020-12-02 05:54:34 -0500

    layout completed

--- src/Components/Feeds.jsx
+++ src/Components/Feeds.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import "../Styles/Feeds.css";
+import { fetchUser, fetchAllUsers, fetchExperiences } from "../utils";
+import ProfileDetailsCard from "./ProfileDetailsCard";
 
 export default class Feeds extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Container className="feeds">
+        <Row>
+          <Col md={3}>{/* here goes the profile card - Rita - */}</Col>
+          <Col md={5}> {/* here goes all feeds + create new feed - */}</Col>
+          <Col md={4}> {/* here goes the small list of recent feeds - */}</Col>
+        </Row>
+      </Container>
+    );
+  }
 }
---
+++ src/Components/FeedsProfileCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Col, Row } from "react-bootstrap";
+
+
+function FeedsProfileCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FeedsProfileCard
