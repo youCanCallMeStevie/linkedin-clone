fa0c6f46003c0796fc80cc5b47c8860b9eb3da44
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2020-12-04 13:29:58 +0100

    adds hamburger menu and responsiv. feeds

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,25 @@
 import React, { Component } from "react";
-import {withRouter,Link} from 'react-router-dom'
+import { withRouter, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Linkedin from "../Assets/LinkedIn-Logos/linkedin.png";
+import LiLogo from "../Assets/LinkedIn-Logos/LI-Logo.png";
 import "../Styles/navbar.css";
-import {
-  Navbar,
-  Row,
-  Col,  
-  Container,
-  Nav,
-  NavDropdown,
-} from "react-bootstrap";
-import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
-
-import HomeIcon from '@material-ui/icons/Home';
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import WorkIcon from '@material-ui/icons/Work';
-import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
+import { Navbar, Row, Col, Container, Nav, NavDropdown, Dropdown } from "react-bootstrap";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from "@material-ui/icons/Home";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import WorkIcon from "@material-ui/icons/Work";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
 import AppsOutlinedIcon from "@material-ui/icons/AppsOutlined";
-import { Avatar } from '@material-ui/core';
+import { Avatar } from "@material-ui/core";
 import withUser from "./withUser";
+import "../Styles/ham-navbar.css"
 
-
-function NavBar({currentUser,history }) {
+function NavBar({ currentUser, history }) {
   // state = {
   //   user:{}
   // }
@@ -37,139 +31,166 @@ function NavBar({currentUser,history }) {
   //   }
   // }
 
-
   return (
-      <div>
-        <Navbar expand="lg" className=" mynavbar">
-          <Container>
-            <Navbar.Brand href="/feeds" className=" position-sticky logo">
-              <img alt="icon" src={Linkedin} className="logo" />
-            </Navbar.Brand>
-            <div className="search">
-              <div className="searchIcon">
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: "inputRoot",
-                  input: " inputInput",
-                }}
-                inputProps={{ "aria-label": "search" }}
-              />
-            </div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
-                <div className="navbar-item">
-                  <Nav.Link href="/feeds">
-                    <HomeIcon className="icons" />
-                    Home
-                  </Nav.Link>
-                </div>
-                <div className="navbar-item">
-                  <Nav.Link href="#network">
-                    <PeopleAltIcon className="icons" />
-                    My Network
-                  </Nav.Link>
-                </div>
-                <div className="navbar-item">
-                  <Nav.Link href="#jobs">
-                    <WorkIcon className="icons" />
-                    Jobs
-                  </Nav.Link>
-                </div>
-                <div className="navbar-item">
-                  <Nav.Link href="#messages">
-                    <ChatBubbleIcon className="icons" />
-                    Messaging
-                  </Nav.Link>
-                </div>
-                <div className="navbar-item">
-                  <Nav.Link href="#notif">
-                    <NotificationsIcon className="icons" />
-                    Notifications
-                  </Nav.Link>
-                </div>
-                <div className="navbar-itemMe">
-                  <Avatar
-                    className="navbar-avatar"
-                    alt={currentUser?.image}
-                    src={currentUser?.image}
-                  />
+    <div>
+      <Nav>
+      <Container className="fluid ml-5">
+      <Dropdown className=" hamburger-menu d-fixed">
+      <img alt="icon" src={LiLogo} className="lilogo" />
+        <Dropdown.Toggle className="text-right" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" variant="mycolor">
+         <MenuIcon/>
+        </Dropdown.Toggle>
 
-                  <NavDropdown
-                    title="Me"
-                    id="basic-nav-dropdown"
-                    className="avatar-dd"
+        <Dropdown.Menu aria-labelledby="navbarDropdown">
+          <Dropdown.Item href="/feeds"></Dropdown.Item>
+          <Dropdown.Item href="/profile/me"><Avatar
+                  className="navbar-avatar"
+                  alt={currentUser?.image}
+                  src={currentUser?.image}
+                /></Dropdown.Item>
+          <Dropdown.Item href="#/action-3"><WorkIcon className="icons" /></Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      </Container>
+      </Nav>
+      <Navbar expand="lg" className=" mynavbar">
+        <Container>
+          <Navbar.Brand href="/feeds" className=" position-sticky logo">
+            <img alt="icon" src={Linkedin} className="logo" />
+          </Navbar.Brand>
+          <div className="search">
+            <div className="searchIcon">
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: "inputRoot",
+                input: " inputInput",
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <div className="navbar-item">
+                <Nav.Link href="/feeds">
+                  <HomeIcon className="icons" />
+                  Home
+                </Nav.Link>
+              </div>
+              <div className="navbar-item">
+                <Nav.Link href="#network">
+                  <PeopleAltIcon className="icons" />
+                  My Network
+                </Nav.Link>
+              </div>
+              <div className="navbar-item">
+                <Nav.Link href="#jobs">
+                  <WorkIcon className="icons" />
+                  Jobs
+                </Nav.Link>
+              </div>
+              <div className="navbar-item">
+                <Nav.Link href="#messages">
+                  <ChatBubbleIcon className="icons" />
+                  Messaging
+                </Nav.Link>
+              </div>
+              <div className="navbar-item">
+                <Nav.Link href="#notif">
+                  <NotificationsIcon className="icons" />
+                  Notifications
+                </Nav.Link>
+              </div>
+              <div className="navbar-itemMe">
+                <Avatar
+                  className="navbar-avatar"
+                  alt={currentUser?.image}
+                  src={currentUser?.image}
+                />
+
+                <NavDropdown
+                  title="Me"
+                  id="basic-nav-dropdown"
+                  className="avatar-dd"
+                >
+                  <Row>
+                    <Col lg={2}>
+                      <Avatar
+                        className="navbar-avatar"
+                        alt={currentUser?.image}
+                        src={currentUser?.image}
+                      />
+                    </Col>
+                    <Col lg={10}>
+                      <p className="name-title">
+                        <strong>
+                          {currentUser?.name} {currentUser?.surname}
+                        </strong>
+                      </p>
+                      <p className="name-title">{currentUser.title}</p>
+                    </Col>
+                  </Row>
+                  <button
+                    className="button-profile"
+                    onClick={() => history.push("/profile/me")}
                   >
-                    <Row>
-                      <Col lg={2}>
-                        <Avatar
-                          className="navbar-avatar"
-                          alt={currentUser?.image}
-                          src={currentUser?.image}
-                        />
-                      </Col>
-                      <Col lg={10}>
-                        <p className="name-title">
-                          <strong>
-                            {currentUser?.name} {currentUser?.surname}
-                          </strong>
-                        </p>
-                        <p className="name-title">{currentUser.title}</p>
-                      </Col>
-                    </Row>
-                    <button className="button-profile" onClick={()=>history.push('/profile/me')}>View profile</button>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.3">
-                      <p>
-                        <strong>Account</strong>
-                      </p>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.4">
-                      Settings & Privacy
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.4">Help</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.4">
-                      Language
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item
-                      className="dropdown-item-strong"
-                      href="#action/3.4"
-                    >
-                      <p>
-                        <strong>Manage</strong>
-                      </p>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.4">
-                      Posts & Activity
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.4">
-                      Job Posting Account
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                      Sign out
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </div>
-                <div className="navbar-item navbar-item-work ">
-                  <Nav.Link href="#work">
-                    <AppsOutlinedIcon className="icons" />
-                    Work
-                  </Nav.Link>
-                </div>
-                <div className="navbar-item">
-                  <Nav.Link href="#learning"><MenuBookIcon className="icons"/>Linkedin Learning</Nav.Link>
-                </div>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
-  )
+                    View profile
+                  </button>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.3">
+                    <p>
+                      <strong>Account</strong>
+                    </p>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">
+                    Settings & Privacy
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">Help</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">
+                    Language
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item
+                    className="dropdown-item-strong"
+                    href="#action/3.4"
+                  >
+                    <p>
+                      <strong>Manage</strong>
+                    </p>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">
+                    Posts & Activity
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">
+                    Job Posting Account
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Sign out
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </div>
+              <div className="navbar-item navbar-item-work ">
+                <Nav.Link href="#work">
+                  <AppsOutlinedIcon className="icons" />
+                  Work
+                </Nav.Link>
+              </div>
+              <div className="navbar-item">
+                <Nav.Link href="#learning">
+                  <MenuBookIcon className="icons" />
+                  Linkedin Learning
+                </Nav.Link>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default withUser(withRouter(NavBar));
