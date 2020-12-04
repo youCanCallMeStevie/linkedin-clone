26c7019670912ba1cf4b529efcc666ee407d5000
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2020-12-04 10:36:20 -0500

    css fixes

--- src/Components/Homefooter.jsx
+++ src/Components/Homefooter.jsx
@@ -8,7 +8,7 @@ import {
 export default class HomeFooter extends Component {
     render() {
         return (
-            <>
+            <div className="home-footer">
                 <Row>
                     <Col>About</Col>
                     <Col>Accessibility</Col>
@@ -23,7 +23,7 @@ export default class HomeFooter extends Component {
                     <Col>Business Services</Col>
                     <Col>Get the Linkedin pp</Col>
                 </Row>
-            </>
+            </div>
         )
     }
 }
